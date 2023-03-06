1bde1cd3bed8ee9274db53efb023df240206c8c5
unknown <atl.mihalmdazmaeen@gmail.com>
2023-03-06 16:07:56 +0600

    experience

--- resources/views/front/pages/experience.blade.php
+++ resources/views/front/pages/experience.blade.php
@@ -1,15 +1,16 @@
 @extends('front.layouts.front_app');
 @section('front_content')
 
+    <div class="py-3">
     <!-- Start Hero Area -->
-    <section class="hero-area   bg-white" id="project_done">
+    <section class="hero-area bg-white" id="project_done">
         <div class="">
             <div class="row m-0">
                 <!-- Hero Content -->
                 <div class="col-lg-6 order-lg-first order-last align-self-center d-flex justify-content-center ">
                     <div class="hero-caption w-75">
-                        {{-- <h4 class="left-border-s text-white">{{ __('Banks and Financial Institution
-                            ') }}</h4> --}}
+                        <h4 class="left-border-s text-dark">{{ __('Experience
+                            ') }}</h4>
                         <h2 class=" text-text-dark">Banks and Financial Institution</h2>
                        <ul style="list-style:circle;color:rgb(19, 19, 19);padding:10px 0px 10px 45px">
                         <li>The Hongkong and Shanghai Banking Corporation Ltd.(HSBC), Bangladesh Branch </li>
@@ -23,10 +24,8 @@
                 <!-- Hero Image -->
                 <div class="col-lg-6 align-self-end p-0">
                     <div class="hero-image">
-                        <img src="{{ asset($sliders->photo) }}" alt="img" class="">
-
+                        <img src="{{ asset('uploads/experience/bank-financial-ins.jpg') }}" alt="img" class="">
                     </div>
-
                 </div>
             </div>
         </div>
@@ -35,13 +34,13 @@
 
 
     <!-- Start Hero Area -->
-    <section class="hero-area   bg-white" id="project_done">
+    <section class="hero-area bg-white" id="project_done">
             <div class="row m-0">
                 <!-- Hero Content -->
                  <!-- Hero Image -->
                  <div class="col-lg-6 p-0">
                     <div class="hero-image">
-                        <img src="{{ asset($sliders->photo) }}" alt="img" class="">
+                        <img src="{{ asset('uploads/experience/communication.jpg') }}" alt="img" class="">
 
                     </div>
 
@@ -49,15 +48,19 @@
 
                 <div class="col-lg-6  d-flex align-items-center justify-content-center">
                     <div class="hero-caption w-75">
-                        {{-- <h4 class="left-border-s text-white">{{ __('Banks and Financial Institution
-                            ') }}</h4> --}}
-                        <h2 class=" text-dark"> Banks and Financial Institution </h2>
+                        <h4 class="left-border-s text-dark">{{ __('Experience
+                            ') }}</h4>
+                        <h2 class=" text-dark">Communication</h2>
                        <ul style="list-style:circle;color:rgb(8, 8, 8);padding:10px 0px 10px 45px">
-                        <li>The Hongkong and Shanghai Banking Corporation Ltd.(HSBC), Bangladesh Branch </li>
-                        <li>CitiBank N.A.</li>
-                        <li>Standard Chartered Bank</li>
-                        <li>IDLC finance limited </li>
-                        <li>IPDC </li>
+                        <li>Orascom Telecom Bangladesh Limited </li>
+                        <li>Alcatel</li>
+                        <li>Ericsson</li>
+                        <li>Grameen Phone Ltd.</li>
+                        <li>IBM India Pvt. Ltd. </li>
+                        <li>Motorola </li>
+                        <li>Siemens Bangladesh Limited </li>
+                        <li>Warid Telecom International Limited </li>
+                        <li>WorldTel Bangladesh Ltd.</li>
                        </ul>
                    </div>
                 </div>
@@ -68,28 +71,38 @@
 
 
         <!-- Start Hero Area -->
-        <section class="hero-area   bg-white" id="project_done">
+        <section class="hero-area bg-white" id="project_done">
             <div class="">
                 <div class="row m-0">
                     <!-- Hero Content -->
                     <div class="col-lg-6 order-lg-first order-last align-self-center d-flex justify-content-center ">
                         <div class="hero-caption w-75">
-                            {{-- <h4 class="left-border-s text-white">{{ __('Banks and Financial Institution
-                                ') }}</h4> --}}
-                            <h2 class=" text-dark"> Banks and Financial Institution </h2>
+                            <h4 class="left-border-s text-dark">{{ __('Experience
+                                ') }}</h4>
+                            <h2 class=" text-dark"> Manufacturing </h2>
                            <ul style="list-style:circle;color:rgb(35, 35, 35);padding:10px 0px 10px 45px">
-                            <li>The Hongkong and Shanghai Banking Corporation Ltd.(HSBC), Bangladesh Branch </li>
-                            <li>CitiBank N.A.</li>
-                            <li>Standard Chartered Bank</li>
-                            <li>IDLC finance limited </li>
-                            <li>IPDC </li>
+                            <li>Advanced Chemical Industries Ltd. (ACI) </li>
+                            <li>Bayer CropScience Ltd</li>
+                            <li>Aventis Limited</li>
+                            <li>BASF Bangladesh Ltd </li>
+                            <li>Bata Shoe Co. (Bangladesh) Ltd. </li>
+                            <li>BOC Bangladesh Ltd.</li>
+                            <li>British American Tobacco Bangladesh Company Ltd</li>
+                            <li>Cemex Cement Bangladesh Ltd.</li>
+                            <li>Coats Bangladesh Ltd.</li>
+                            <li>Holcim (Bangladesh) Ltd.</li>
+                            <li>Integrated Beverage Services Bangladesh Ltd.</li>
+                            <li>Karnaphuli Fertilizer Company Ltd.</li>
+                            <li>Nestle Bangladesh Limited</li>
+                            <li>Renata Limited</li>
+                            <li>Scancement International Ltd.</li>
                            </ul>
                        </div>
                     </div>
                     <!-- Hero Image -->
                     <div class="col-lg-6 align-self-end p-0">
                         <div class="hero-image">
-                            <img src="{{ asset($sliders->photo) }}" alt="img" class="">
+                            <img src="{{ asset('uploads/experience/manufacturing.jpg') }}" alt="img" class="">
 
                         </div>
 
@@ -98,4 +111,320 @@
             </div>
         </section>
         <!-- End Hero Area -->
+
+         <!-- Start Hero Area -->
+    <section class="hero-area   bg-white" id="project_done">
+        <div class="row m-0">
+            <!-- Hero Content -->
+             <!-- Hero Image -->
+             <div class="col-lg-6 p-0">
+                <div class="hero-image">
+                    <img src="{{ asset('uploads/experience/energy-power.jpg') }}" alt="img" class="">
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-6  d-flex align-items-center justify-content-center">
+                <div class="hero-caption w-75">
+                    <h4 class="left-border-s text-dark">{{ __('Experience
+                        ') }}</h4>
+                    <h2 class=" text-dark">Energy and Power </h2>
+                   <ul style="list-style:circle;color:rgb(8, 8, 8);padding:10px 0px 10px 45px">
+                    <li>BP Middle East Ltd.</li>
+                    <li>Cairn Energy PLC</li>
+                    <li>Chevron Bangladesh</li>
+                    <li>Halliburton International, Inc</li>
+                    <li>Haripur Power Limited</li>
+                    <li>Kellog Brown and Root Bangladesh Ltd. </li>
+                    <li>Khulna Power Company Ltd. </li>
+                    <li>Niko Resources (Bangladesh) Ltd. </li>
+                    <li>Wartsila Bangladesh Limited</li>
+                   </ul>
+               </div>
+            </div>
+        </div>
+</section>
+<!-- End Hero Area -->
+
+ <!-- Start Hero Area -->
+ <section class="hero-area   bg-white" id="project_done">
+    <div class="">
+        <div class="row m-0">
+            <!-- Hero Content -->
+            <div class="col-lg-6 order-lg-first order-last align-self-center d-flex justify-content-center ">
+                <div class="hero-caption w-75">
+                    <h4 class="left-border-s text-dark">{{ __('Experience
+                        ') }}</h4>
+                    <h2 class=" text-dark"> Construction </h2>
+                   <ul style="list-style:circle;color:rgb(35, 35, 35);padding:10px 0px 10px 45px">
+                    <li>Chiyoda Corporation </li>
+                    <li>Mitsui Construction Co. Ltd.</li>
+                    <li>Degremont SA, France</li>
+                    <li>Hanil Construction Co. Ltd. </li>
+                    <li>Hyundai Engineering & Construction Co. Ltd. </li>
+                    <li>Samwhan Corporation</li>
+                    <li>Shimizu Construction Co. Ltd.</li>
+                    <li>Toyo Engineering Corporation</li>
+                   </ul>
+               </div>
+            </div>
+            <!-- Hero Image -->
+            <div class="col-lg-6 align-self-end p-0">
+                <div class="hero-image">
+                    <img src="{{ asset('uploads/experience/construction.jpg') }}" alt="img" class="">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Area -->
+
+<!-- Start Hero Area -->
+<section class="hero-area   bg-white" id="project_done">
+    <div class="row m-0">
+        <!-- Hero Content -->
+         <!-- Hero Image -->
+         <div class="col-lg-6 p-0">
+            <div class="hero-image">
+                <img src="{{ asset('uploads/experience/trading.jpg') }}" alt="img" class="">
+
+            </div>
+
+        </div>
+
+        <div class="col-lg-6  d-flex align-items-center justify-content-center">
+            <div class="hero-caption w-75">
+                <h4 class="left-border-s text-dark">{{ __('Experience
+                    ') }}</h4>
+                <h2 class=" text-dark">Trading </h2>
+               <ul style="list-style:circle;color:rgb(8, 8, 8);padding:10px 0px 10px 45px">
+                <li>Kanematsu-Gosho Ltd., Bangladesh branch</li>
+                <li>Marubeni Corporation, Bangladesh branch</li>
+                <li>Mitsui & Co. Ltd., Bangladesh branch</li>
+                <li>Sumitomo Corporation, Bangladesh branch</li>
+                <li>Toyota Tsusho Corporation, Bangladesh branch</li>
+                <li>Associated Merchandising Corporation </li>
+                <li>Procter & Gamble Bangladesh Private Ltd.</li>
+               </ul>
+           </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Area -->
+
+<!-- Start Hero Area -->
+<section class="hero-area   bg-white" id="project_done">
+    <div class="">
+        <div class="row m-0">
+            <!-- Hero Content -->
+            <div class="col-lg-6 order-lg-first order-last align-self-center d-flex justify-content-center ">
+                <div class="hero-caption w-75">
+                    <h4 class="left-border-s text-dark">{{ __('Experience
+                        ') }}</h4>
+                    <h2 class=" text-dark"> Others </h2>
+                   <ul style="list-style:circle;color:rgb(35, 35, 35);padding:10px 0px 10px 45px">
+                    <li>Dhaka Sheraton Hotel </li>
+                    <li>Grey International Bangladesh Ltd</li>
+                    <li>Jibtex Ltd</li>
+                    <li>Mondial Orient Limited </li>
+                    <li>P&O Nedlloyd Bangladesh Ltd. </li>
+                    <li>Sainsbury‟s Asia Limited</li>
+                    <li>Sears Buying Services, Inc.</li>
+                    <li>Pan Pacific Sonargaon Hotel</li>
+                   </ul>
+               </div>
+            </div>
+            <!-- Hero Image -->
+            <div class="col-lg-6 align-self-end p-0">
+                <div class="hero-image">
+                    <img src="{{ asset('uploads/experience/others.jpg') }}" alt="img" class="">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Area -->
+
+<!-- Start Hero Area -->
+<section class="hero-area   bg-white" id="project_done">
+    <div class="row m-0">
+        <!-- Hero Content -->
+         <!-- Hero Image -->
+         <div class="col-lg-6 p-0">
+            <div class="hero-image">
+                <img src="{{ asset('uploads/experience/garments.jpg') }}" alt="img" class="">
+
+            </div>
+
+        </div>
+
+        <div class="col-lg-6  d-flex align-items-center justify-content-center">
+            <div class="hero-caption w-75">
+                <h4 class="left-border-s text-dark">{{ __('Existence
+                    ') }}</h4>
+                <h2 class=" text-dark">Garments </h2>
+               <ul style="list-style:circle;color:rgb(8, 8, 8);padding:10px 0px 10px 45px">
+                <li>Arabi Fashion Ltd.</li>
+                <li>Binoda Knitwear Ltd</li>
+                <li>Appear Composite Fashion Ltd.</li>
+                <li>Rose Valley Fashion Ltd</li>
+                <li>Bengal Hurricane Limited</li>
+                <li>Novel Hurricane and Knit Garments Ltd </li>
+                <li>AST Knitwear Ltd</li>
+                <li>Cotton Uro Asia Ltd</li>
+                <li>Akzochem Industries Ltd.</li>
+                <li>Homme Tex International Ltd.</li>
+                <li>Elegence Woven Level Industries Ltd.</li>
+               </ul>
+           </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Area -->
+<!-- Start Hero Area -->
+<section class="hero-area   bg-white" id="project_done">
+    <div class="">
+        <div class="row m-0">
+            <!-- Hero Content -->
+            <div class="col-lg-6 order-lg-first order-last align-self-center d-flex justify-content-center ">
+                <div class="hero-caption w-75">
+                    <h4 class="left-border-s text-dark">{{ __('Existence
+                        ') }}</h4>
+                    <h2 class=" text-dark"> Construction </h2>
+                   <ul style="list-style:circle;color:rgb(35, 35, 35);padding:10px 0px 10px 45px">
+                    <li>Posco International Corporation</li>
+                    <li>Korea Engineering Consultancy Corporation (KECC)</li>
+                    <li>Samwhan Corporation</li>
+                    <li>S. J. Construction and Trade Ltd. </li>
+                    <li>Dongfang Electric International Corporation </li>
+                    <li>Adam Smith International Ltd.</li>
+                    <li>Momen Real Estate Ltd.</li>
+                    <li>Momen Infrastructure Ltd.</li>
+                   </ul>
+               </div>
+            </div>
+            <!-- Hero Image -->
+            <div class="col-lg-6 align-self-end p-0">
+                <div class="hero-image">
+                    <img src="{{ asset('uploads/experience/construction1.jpg') }}" alt="img" class="">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Area -->
+<!-- Start Hero Area -->
+<section class="hero-area   bg-white" id="project_done">
+    <div class="row m-0">
+        <!-- Hero Content -->
+         <!-- Hero Image -->
+         <div class="col-lg-6 p-0">
+            <div class="hero-image">
+                <img src="{{ asset('uploads/experience/manufacturing1.jpg') }}" alt="img" class="">
+
+            </div>
+
+        </div>
+
+        <div class="col-lg-6  d-flex align-items-center justify-content-center">
+            <div class="hero-caption w-75">
+                <h4 class="left-border-s text-dark">{{ __('Existence
+                    ') }}</h4>
+                <h2 class=" text-dark">Manufacturing </h2>
+               <ul style="list-style:circle;color:rgb(8, 8, 8);padding:10px 0px 10px 45px">
+                <li>Advanced Animal Science Ltd</li>
+                <li>Karnaphuli Fertilizer Company Ltd.</li>
+                <li>Akota Agro Ltd.</li>
+                <li>Bicyle Shop Ltd</li>
+                <li>Boond Engineering Bangladesh Pvt. Limited</li>
+                <li>Boond SPV-1 Bangladesh Pvt. Limited </li>
+                <li>Naafco Pharma Ltd.</li>
+                <li>Naafco Agrovet Ltd.</li>
+                <li>Naafco (Pvt.) Ltd.</li>
+                <li>Semco Energy Ltd.</li>
+                <li>Semco Corporation Ltd.</li>
+                <li>AG Bricks Ltd</li>
+               </ul>
+           </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Area -->
+<!-- Start Hero Area -->
+<section class="hero-area   bg-white" id="project_done">
+    <div class="">
+        <div class="row m-0">
+            <!-- Hero Content -->
+            <div class="col-lg-6 order-lg-first order-last align-self-center d-flex justify-content-center ">
+                <div class="hero-caption w-75">
+                    <h4 class="left-border-s text-dark">{{ __('Existence
+                        ') }}</h4>
+                    <h2 class=" text-dark">Energy and Power  </h2>
+                   <ul style="list-style:circle;color:rgb(35, 35, 35);padding:10px 0px 10px 45px">
+                    <li>Wartsila Bangladesh Limited</li>
+                    <li>Dongfang Electric International Corporation</li>
+                    <li>Dupont BD Ltd.</li>
+                    <li>Akota Electric Ltd</li>
+                    <li>Akota Pole Industries Ltd.</li>
+                   </ul>
+               </div>
+            </div>
+            <!-- Hero Image -->
+            <div class="col-lg-6 align-self-end p-0">
+                <div class="hero-image">
+                    <img src="{{ asset('uploads/experience/energy-power1.jpg') }}" alt="img" class="">
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Hero Area -->
+<!-- Start Hero Area -->
+<section class="hero-area   bg-white" id="project_done">
+    <div class="row m-0">
+        <!-- Hero Content -->
+         <!-- Hero Image -->
+         <div class="col-lg-6 p-0">
+            <div class="hero-image">
+                <img src="{{ asset('uploads/experience/others1.jpg') }}" alt="img" class="">
+
+            </div>
+
+        </div>
+
+        <div class="col-lg-6  d-flex align-items-center justify-content-center">
+            <div class="hero-caption w-75">
+                <h4 class="left-border-s text-dark">{{ __('Existence
+                    ') }}</h4>
+                <h2 class=" text-dark">Others </h2>
+               <ul style="list-style:circle;color:rgb(8, 8, 8);padding:10px 0px 10px 45px">
+                <li>Pan Pacific Hotel and Resorts </li>
+                <li>Health Labs Ltd.</li>
+                <li>Freight Zone Ltd.</li>
+                <li>Alliance Management Services Ltd</li>
+                <li>Wasseq Dredging Limited</li>
+                <li>Astra Bio Pharmaceuticals Ltd. </li>
+                <li>Trauma Centre and AO Orthopedic Hospital Ltd</li>
+                <li>Prabat Services Ltd. </li>
+                <li>Prabat Social Infrastructure Ltd</li>
+                <li>Anfords Bangladesh Ltd.</li>
+                <li>Zaman Pharmaceuticals Ltd.</li>
+                <li>Fakhrul Islam Securities Ltd.</li>
+               </ul>
+           </div>
+        </div>
+    </div>
+</section>
+</div>
+<!-- End Hero Area -->
+
 @endsection
